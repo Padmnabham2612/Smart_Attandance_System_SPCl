9c31bec10f8a168655741aacd2979a63feb61eb6
anjali <kanaseanjali1@gmail.com>
2026-05-08 19:25:25 +0530

    fixed backend and added working code

--- register/src/main/java/com/example/attendance_Backend/repository/NotesRepository.java
+++ register/src/main/java/com/example/attendance_Backend/repository/NotesRepository.java
@@ -3,13 +3,16 @@ package com.example.attendance_Backend.repository;
 import com.example.attendance_Backend.model.Notes;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
-import java.util.Optional;
-
 public interface NotesRepository extends JpaRepository<Notes, Long> {
+
     List<Notes> findByAdminId(Long adminId);
 
     List<Notes> findByDepartment_IdAndClassMaster_IdAndDivisionMaster_IdAndAdminId(
             Integer departmentId, Integer classId, Integer divisionId, Long adminId);
+
+    // ✅ ADD THIS METHOD
+    List<Notes> findByUploadTimeBefore(LocalDateTime uploadTime);
 }
